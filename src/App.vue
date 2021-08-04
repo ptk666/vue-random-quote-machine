16e25768d83e4e2d75a9aace764773b3818311ed
ptk666 <potingkay@gmail.com>
2021-08-04 16:33:23 +0800

    successfully added text & quotes in QuoteBox.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <QuoteBox
+    :quote='quotes' />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import QuoteBox from './components/QuoteBox.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    QuoteBox
+  },
+  data() {
+    return {
+      quotes: {
+        text: '',
+        author: ''
+      }
+    }
+  },
+  mounted() {
+    this.getQuotes();
+  },
+  methods: {
+    async getQuotes() {
+      try {
+        const res = await axios.get('https://goquotes-api.herokuapp.com/api/v1/random?count=1');
+        this.quotes = res.data.quotes[0];
+      }
+      catch(e) {
+        console.log(e.message);
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap');
+
+  body {
+    font-family: 'Roboto Mono', monospace;
+    height: 100vh;
+  }
+
+
 </style>
---
+++ src/components/QuoteBox.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id='quote-box'>
+    <p id="text">"{{quote.text}}"</p>
+    <p id="author">— <em>{{quote.author}}</em></p>
+  </div>
+</template>
+
+<script>
+
+export default {
+name: 'QuoteBox',
+props: ['quote']
+}
+</script>
+
+<style>
+  #quote-box {
+    background-color: red;
+    width: 50%;
+    border-radius: 10px;
+    justify-content: center;
+    text-align: center;
+    margin: 0 auto;
+    margin-top: 5em;
+    padding: 50px;
+  }
+
+  p, #text {
+    font-weight: 700;
+    font-size: 20px;
+    color: white;
+  }
+
+  p, #author {
+    font-size: 13px;
+    color: white;
+  }
+</style>
